8d36c4b16a016a14d3113af769b56e8ff8c02e7b
Max Wood <max.w29@hotmail.com>
2021-11-12 12:21:03 -0600

    fixing deployed issue not processing pdf's

--- streamlit/utils.py
+++ streamlit/utils.py
@@ -370,15 +370,15 @@ def empty_data_folder():
     return
 
 def save_uploaded_file(uploadedfile):
-    base_dir = '/app/paper-grading-assistant/streamlit'
-    try:
-        if st.secrets['prod_env']:
-            dir_name = base_dir + "/data/"
-        else:
-            dir_name = "./data/"
-    except:
-            dir_name = "./data/"
-    # dir_name = "./data"
+    # base_dir = '/app/paper-grading-assistant/streamlit'
+    # try:
+    #     if st.secrets['prod_env']:
+    #         dir_name = base_dir + "/data/"
+    #     else:
+    #         dir_name = "./data/"
+    # except:
+    #         dir_name = "./data/"
+    dir_name = "./data"
     filename = os.path.join(dir_name,uploadedfile.name)
     with open(filename,"wb") as f:
         f.write(uploadedfile.getbuffer())
